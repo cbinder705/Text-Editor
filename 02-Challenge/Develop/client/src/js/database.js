9c6650907fcad9e80e26e0441a7909683bc31319
Cory Bee <cbinder705@gmail.com>
2023-01-17 23:51:05 -0500

    updated database.js with get routes

--- 02-Challenge/Develop/client/src/js/database.js
+++ 02-Challenge/Develop/client/src/js/database.js
@@ -15,8 +15,8 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (id, content) => {
   console.log("PUT to the database");
-  const todosDb = await openDB("jate", 1);
-  const tx = todosDb.transaction("jate", "readwrite");
+  const JateDb = await openDB("jate", 1);
+  const tx = JateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
   const request = store.put({ id: 1, todo: content });
   const result = await request;
@@ -24,6 +24,25 @@ export const putDb = async (id, content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+  console.log("GET from the database");
+
+  // Create a connection to the database database and version we want to use.
+  const JateDb = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = JateDb.transaction("jate", "readonly");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
 
 initdb();
